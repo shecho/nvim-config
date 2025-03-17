dca859320d5785f0d077b4ddafb743351f231668
shecho <shechovala@gmail.com>
2025-03-17 17:44:11 -0500

    remove telescpoe keymaps, add telescp keymaps on keymaps file

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -20,40 +20,38 @@ return {
     -- { "nvim-telescope/telescope-media-files.nvim" },
     -- { "danielvolchek/tailiscope.nvim" },
   },
+  keys = {
+    -- {
+    --   "n",
+    --   "<leader>fd",
+    --   "<cmd>Telescope find_files theme=dropdown layout_strategy=vertical layout_config={prompt_position='top',width=0.99,height=0.99,vertical={preview_height=0.50},horizontal={preview_height=0.46}}<cr>",
+    --   { noremap = true, silent = true, desc = "find files" },
+    -- },
+    -- {
+    --   "n",
+    --   "<leader>p",
+    --   "<cmd>Telescope find_files hidden=true no_ignore=true layout_config={width=0.99,height=0.99}<cr>",
+    --   { noremap = true, silent = true, desc = "Fuzzy find files" },
+    -- },
+    -- { "n", "<localleader>b", "<cmd>Telescope buffers initial_mode=normal<cr>", { noremap = true, silent = true, desc = "Buffers" } },
+    -- { "n", "<localleader>o", "<cmd>Telescope oldfiles initial_mode=normal<cr>", { noremap = true, silent = true, desc = "Recent files" } },
+    -- { "n", "<leader>sP", "<cmd>Telescope projects theme=dropdown initial_mode=normal <cr>", { noremap = true, silent = true, desc = "Projects" } },
+  },
   opts = function()
     local telescope = require("telescope")
-    -- local builtin = require("telescope.builtin")
-    -- local add_to_trouble = require("trouble.sources.telescope").add
-    -- local trouble = require("trouble.sources.telescope")
-
     local actions = require("telescope.actions")
     local icons = require("user.icons")
 
     telescope.load_extension("fzf")
     telescope.load_extension("projects")
     -- telescope.load_extension("egrepify")
-
     -- telescope.load_extension("harpoon")
     -- telescope.load_extension("ui-select")
     -- telescope.load_extension("file_browser")
     -- telescope.load_extension("media_files")
     -- telescope.load_extension("tailiscope")
-    local keymap = vim.keymap
-    -- keymap.set(
-    --   "n",
-    --   "<leader>fd",
-    --   "<cmd>Telescope find_files theme=dropdown layout_strategy=vertical layout_config={prompt_position='top',width=0.99,height=0.99,vertical={preview_height=0.50},horizontal={preview_height=0.46}}<cr>",
-    --   { desc = "find files" }
-    -- )
-    -- keymap.set("n", "<leader>p", "<cmd>Telescope find_files hidden=true no_ignore=true layout_config={width=0.99,height=0.99}<cr>", { desc = "Fuzzy find files" })
-    -- keymap.set("n", "<leader>sf", "<cmd>Telescope file_browser initial_mode=normal theme=ivy<cr>", { desc = "Find files" })
-    -- keymap.set("n", "<leader>sb", "<cmd>Telescope buffers initial_mode=normal<cr>", { desc = "Buffers" })
-    -- keymap.set("n", "<leader>sm", "<cmd>Telescope marks initial_mode=normal<cr>", { desc = "Marks" })
-    -- keymap.set("n", "<leader>so", "<cmd>Telescope oldfiles initial_mode=normal<cr>", { desc = "Recent files" })
-    -- keymap.set("n", "<leader>sa", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    -- keymap.set("n", "<leader>sw", "<cmd>Telescope grep_string<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>sP", "<cmd>Telescope projects theme=dropdown initial_mode=normal <cr>", { desc = "Projects" })
-
+    -- local keymap = vim.keymap
+    --
     return {
       defaults = {
         layout_config = {
@@ -116,24 +114,17 @@ return {
           i = {
             ["<C-n>"] = actions.cycle_history_next,
             ["<C-p>"] = actions.cycle_history_prev,
-
             ["<C-j>"] = actions.move_selection_next,
             ["<D-j>"] = actions.move_selection_next,
             ["<C-k>"] = actions.move_selection_previous,
             ["<D-k>"] = actions.move_selection_previous,
             ["<C-l>"] = actions.select_default,
-
             ["<Down>"] = actions.move_selection_next,
             ["<Up>"] = actions.move_selection_previous,
-
             ["<CR>"] = actions.select_default,
             ["<C-s>"] = actions.select_horizontal,
             ["<C-v>"] = actions.select_vertical,
-            -- ["<C-t>"] = actions.select_tab,
-            -- ["<c-t>"] = trouble.open_with_trouble,
-
             ["<c-d>"] = actions.delete_buffer,
-
             ["<Tab>"] = actions.move_selection_next,
             ["<S-Tab>"] = actions.move_selection_previous,
             ["<C-h>"] = actions.which_key,
@@ -141,7 +132,6 @@ return {
           },
 
           n = {
-            -- ["<c-t>"] = trouble.open,
             ["<esc>"] = actions.close,
             ["<C-j>"] = actions.select_default,
             ["<l>"] = actions.select_default,
@@ -150,11 +140,7 @@ return {
             ["<C-t>"] = actions.select_tab,
             ["<C-b>"] = actions.results_scrolling_up,
             ["<C-f>"] = actions.results_scrolling_down,
-
             ["<C-l>"] = actions.select_default,
-            -- ["<Tab>"] = actions.close,
-            -- ["<S-Tab>"] = actions.close,
-
             ["j"] = actions.move_selection_next,
             ["k"] = actions.move_selection_previous,
             ["L"] = actions.move_to_bottom,
